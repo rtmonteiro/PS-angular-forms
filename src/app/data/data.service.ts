c52b49f0ae4d30303e8daa524b7fa1e0f7c43034
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2020-02-03 15:57:34 -0300

    HTTP Form Posting and Data Access

---
+++ src/app/data/data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { UserSettings } from './user-settings';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  constructor(private http: HttpClient) { }
+
+  getSubscriptionTypes(): Observable<string[]> {
+    return of(['Monthly', 'Annual', 'Lifetime']);
+  }
+
+  postUserSettingsForm(userSettings: UserSettings): Observable<any> {
+    return this.http.post('https://putsreq.com/CflJIlZK508EE3mHAAN2', userSettings);
+
+    // return of(userSettings);
+  }
+}
